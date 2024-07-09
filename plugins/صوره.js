437ea5c375bd1652eff16a1a2087e1aa41d66728
♯ЅᗩFᏒOT꙯ <168010708+safrotbob2@users.noreply.github.com>
2024-07-10 02:09:42 +0300

    Update صوره.js

--- plugins/صوره.js
+++ plugins/صوره.js
@@ -1,13 +1,19 @@
-import {googleImage} from '@bochilteam/scraper';
-const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text) throw `*استخدم الامر كده يحب ${usedPrefix + command} سفروت*`;
-  //if (m.text.includes('gore') || m.text.includes('cp')|| m.text.includes('porno')|| m.text.includes('Gore')|| m.text.includes('rule')|| m.text.includes('CP')|| m.text.includes('Rule34')) return m.reply('[❗𝐈𝐍𝐅𝐎❗] 𝙽𝙾 𝙿𝚄𝙴𝙳𝙾 𝙴𝙽𝚅𝙸𝙰𝚁 𝙴𝚂𝚃𝙴 𝙲𝙾𝙽𝚃𝙴𝙽𝙸𝙴𝙽𝙳𝙾 𝙴𝚂𝚃𝙰 𝙿𝚁𝙾𝙷𝙸𝙱𝙸𝙳𝙾 𝙴𝙻 𝙶𝚁𝚄𝙿𝙾\n𝚂𝙸 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽 𝚈 𝙳𝙴𝚂𝙴𝙰 𝙰𝙲𝚃𝙸𝚅𝙰𝚁𝙻𝙾𝚂 𝚄𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 #enable modohorny');
-  const res = await googleImage(text);
-  const image = await res.getRandom();
-  const link = image;
-  conn.sendFile(m.chat, link, 'error.jpg', `┏─━═║✠║◦¦🖤¦◦║✠║═━─┓\n『❀┇ₛₐfᵣₒₜ bₒₜ┇❀』\nالـطـلـب: ${text}\nسفروت بوت\n┗─━═║✠║◦¦🖤¦◦║✠║═━─┛`, m);
-};
-handler.help = ['gimage <query>', 'imagen <query>'];
-handler.tags = ['internet', 'tools'];
-handler.command = /^(صوره|image|imagen)$/i;
-export default handler;
+import { googleImage } from '@bochilteam/scraper'
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) throw ` *فين اسم الصوره اللي هبحث عنها استخدم الامر كالتالي* \n${usedPrefix + command} *اسم الصوره* `;
+const prohibited = ['سكس', 'نودز']
+if (prohibited.some(word => m.text.toLowerCase().includes(word))) return m.reply('*طلبك اترفض يحب🦦*')      
+try {
+const res = await googleImage(text)
+let image = res.getRandom()
+let link = image
+conn.sendButton(m.chat, `${mid.buscador}: ${text}`, wm, link, [['صوره تاني', `/imagen ${text}`]], null, null, m )
+//conn.sendFile(m.chat, link, 'error.jpg', `💞 ${mid.buscador}: ${text}`, m)
+} catch (e) {
+console.log(`❗❗ خطأ في ${usedPrefix + command} ❗❗`);
+console.log(e)
+}}
+handler.help = ['gimage <query>', 'imagen <query>']
+handler.tags = ['internet', 'tools']
+handler.command = /^(gimage|image|imagen|jpg|صوره)$/i
+export default handler
