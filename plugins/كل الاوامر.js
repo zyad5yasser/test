b1c5544964d0cd579d5e9403f9c5b15c5145f24b
♯ЅᗩFᏒOT꙯ <168010708+safrotbob2@users.noreply.github.com>
2024-07-10 03:25:55 +0300

    Update كل الاوامر.js

--- plugins/كل الاوامر.js
+++ plugins/كل الاوامر.js
@@ -20,7 +20,7 @@
        let uptime = clockString(_uptime)
    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
    if (!(who in global.db.data.users)) throw `✳️ لم يتم العثور على المستخدم في قاعدة البيانات الخاصة بي`
-   let videoUrl = 'https://telegra.ph/file/09e09b9e0376dcff65ccc.mp4'
+   let videoUrl = 'https://telegra.ph/file/73ef7488ba7f7f3e613ee.mp4'
    let user = global.db.data.users[who]
    let { name, exp, diamond, lastclaim, registered, regTime, age, level, role, warn } = global.db.data.users[who]
    let { min, xp, max } = xpRange(user.level, global.multiplier)
@@ -30,7 +30,7 @@
    let sn = createHash('md5').update(who).digest('hex')
    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length 
    let more = String.fromCharCode(8206)
-   m.react('📰')
+   await conn.sendMessage(m.chat, { react: { text: '📰', key: m.key } })
    let readMore = more.repeat(850) 
    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
    let str = `
