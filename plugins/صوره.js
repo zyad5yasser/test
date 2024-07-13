cff508aa54509e0d02c1215143b582f65e9d4708
safrotbot <safrotbob3@gmail.com>
2024-07-13 15:52:29 +0300

    Update صوره.js

--- plugins/صوره.js
+++ plugins/صوره.js
@@ -1,19 +1,14 @@
 import { googleImage } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw ` *فين اسم الصوره اللي هبحث عنها استخدم الامر كالتالي* \n${usedPrefix + command} *اسم الصوره* `;
-const prohibited = ['سكس', 'نودز']
-if (prohibited.some(word => m.text.toLowerCase().includes(word))) return m.reply('*طلبك اترفض يخول😒*')      
-try {
+if (!text) throw `*[❗خطاء❗] مثال علي الامر ${usedPrefix + command} مايكي*`
+if (m.text.includes('gore') || m.text.includes('cp')|| m.text.includes('porno')|| m.text.includes('Gore')|| m.text.includes('rule')|| m.text.includes('CP')|| m.text.includes('Rule34')) return m.reply('[❗خطاء❗] لا يمكنني إرسال هذا المحتوى ، المجموعة محظورة \n إذا كنت مشرفًا وتريد تنشيطها ، اخبر المطور')  
 const res = await googleImage(text)
-let image = res.getRandom()
+let image = await res.getRandom()
 let link = image
-conn.sendButton(m.chat, `صوره ل: ${text}`, wm, link, [['صوره تاني', `/imagen ${text}`]], null, null, m )
-//conn.sendFile(m.chat, link, 'error.jpg', `💞 صوره ل: ${text}`, m)
-} catch (e) {
-console.log(`❗❗ خطأ في ${usedPrefix + command} ❗❗`);
-console.log(e)
-}}
+conn.sendFile(m.chat, link, 'error.jpg', `🔎 *النتيجه ل:* ${text}\n🔗 *من* ${link}\n🌎 *محرك البحث:* جوجل`, m)}
+//let captionn = `🔎 *𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾 𝙳𝙴:* ${text}\n🔗 *𝙻𝙸𝙽𝙺* ${link}\n🌎 *𝙱𝚄𝚂𝙲𝙰𝙳𝙾𝚁:* Google`
+//conn.sendButton(m.chat, captionn, author, link, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `#imagen ${text}`]], m)}
 handler.help = ['gimage <query>', 'imagen <query>']
 handler.tags = ['internet', 'tools']
-handler.command = /^(gimage|image|imagen|jpg|صوره)$/i
+handler.command = /^(gimage|image|صوره|imagen)$/i
 export default handler
